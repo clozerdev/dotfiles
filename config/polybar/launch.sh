fa081a8002db7a2b4cde412184569809ae52a766
clozerdev <alvarodgonzalo@gmail.com>
2025-09-04 09:12:20 +0200

    fix: new polybar script for detecting different monitors

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -7,8 +7,12 @@ MAIN=$(/usr/bin/polybar -m | grep primary | cut -d: -f1)
 AUX=$(/usr/bin/polybar -m | grep -v primary | cut -d: -f1)
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-MONITOR=$MAIN /usr/bin/polybar main -c ${HOME}/.config/polybar/config.ini &
+[ -n "$MAIN" ] && MONITOR=$MAIN /usr/bin/polybar main -c ${HOME}/.config/polybar/config.ini & disown
 
-for aux in $AUX; do
-	MONITOR=$aux /usr/bin/polybar aux -c ${HOME}/.config/polybar/config.ini &
-done
+echo "$AUX"
+
+if [ -n "$AUX" ]; then
+	for aux in $AUX; do
+		MONITOR=$aux /usr/bin/polybar aux -c ${HOME}/.config/polybar/config.ini & disown
+	done disown
+fi
